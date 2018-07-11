fc63d4340b161752aeccb4219dc438e439d01842
Izakk Camillo <izakkcamillo@Skills-Macbook-Pro-17.local>
2018-07-11 13:25:08 -0400

    Changed the print statement in main.swift

---
+++ model a car/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  model a car
+//
+//  Created by Izakk Camillo on 7/11/18.
+//  Copyright © 2018 Izakk Camillo. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hey, how is it going?")
+
